c19f9505e92fd88a3a54e3b7d6ead3a4985d5fcf
Arno <arnocoomans@hotmail.com>
2017-10-15 18:29:12 +0200

    added wrapper for cube, alose more cubes are added in world

--- src/world/TestWorld.java
+++ src/world/TestWorld.java
@@ -54,120 +54,18 @@ public class TestWorld implements IWorldRules {
     @Override
     public void init(Window window) throws Exception {
         renderer.init(window);
-        float[] positions = new float[]{
-//                // V0
-//                -0.5f, 0.5f, 0.5f,
-//                // V1
-//                -0.5f, -0.5f, 0.5f,
-//                // V2
-//                0.5f, -0.5f, 0.5f,
-//                // V3
-//                0.5f, 0.5f, 0.5f,
-//                // V4
-//                -0.5f, 0.5f, -0.5f,
-//                // V5
-//                0.5f, 0.5f, -0.5f,
-//                // V6
-//                -0.5f, -0.5f, -0.5f,
-//                // V7
-//                0.5f, -0.5f, -0.5f,
 
-                //Face 1 (front)
-                -0.5f, 0.5f, 0.5f, //V0 #0
-                -0.5f, -0.5f, 0.5f, //V1 #1
-                0.5f, -0.5f, 0.5f, //V2 #2
-                0.5f, 0.5f, 0.5f, //V3 #3
+        Cube redCube = new Cube();
 
-                //Face 2 (top)
-                -0.5f, 0.5f, 0.5f, //V0 #4
-                0.5f, 0.5f, 0.5f, //V3 #5
-                -0.5f, 0.5f, -0.5f, //V4 #6
-                0.5f, 0.5f, -0.5f, //V5 #7
+        GameItem cube1= new GameItem(redCube.getMesh());
+        cube1.setScale(0.5f);
+        cube1.setPosition(0, 0, -2);
 
-                //Face 3 (back)
-                -0.5f, 0.5f, -0.5f, //V4 #8
-                0.5f, 0.5f, -0.5f, //V5 #9
-                -0.5f, -0.5f, -0.5f, //V6 #10
-                0.5f, -0.5f, -0.5f, //V7 #11
+        GameItem cube2 = new GameItem(redCube.getMesh());
+        cube2.setScale(0.5f);
+        cube2.setPosition(0, 0, -10);
 
-                //Face 4 (bottom)
-                -0.5f, -0.5f, 0.5f, //V1 #12
-                -0.5f, -0.5f, -0.5f, //V6 #13
-                0.5f, -0.5f, -0.5f, //V7 #14
-                0.5f, -0.5f, 0.5f, //V2 #15
-
-                //Face 5 (left)
-                -0.5f, 0.5f, 0.5f, //V0 #16
-                -0.5f, 0.5f, -0.5f, //V4 #17
-                -0.5f, -0.5f, 0.5f, //V1 #18
-                -0.5f, -0.5f, -0.5f, //V6 #19
-
-                //Face 6 (right)
-                0.5f, 0.5f, 0.5f, //V3 #20
-                0.5f, -0.5f, 0.5f, //V2 #21
-                0.5f, 0.5f, -0.5f, //V5 #22
-                0.5f, -0.5f, -0.5f, //V7 #23
-
-        };
-        float[] colours = new float[]{
-                //Face 1 (front)
-                0.15f, 0.0f, 0.0f,
-                0.15f, 0.0f, 0.0f,
-                0.15f, 0.0f, 0.0f,
-                0.15f, 0.0f, 0.0f,
-
-
-                //Face 2 (top)
-                0.3f, 0.0f, 0.0f,
-                0.3f, 0.0f, 0.0f,
-                0.3f, 0.0f, 0.0f,
-                0.3f, 0.0f, 0.0f,
-
-                //Face 3 (back)
-                0.45f, 0.0f, 0.0f,
-                0.45f, 0.0f, 0.0f,
-                0.45f, 0.0f, 0.0f,
-                0.45f, 0.0f, 0.0f,
-
-                //Face 4 (bottom)
-                0.6f, 0.0f, 0.0f,
-                0.6f, 0.0f, 0.0f,
-                0.6f, 0.0f, 0.0f,
-                0.6f, 0.0f, 0.0f,
-
-
-                //Face 5 (left)
-                0.75f, 0.0f, 0.0f,
-                0.75f, 0.0f, 0.0f,
-                0.75f, 0.0f, 0.0f,
-                0.75f, 0.0f, 0.0f,
-
-                //Face 6 (right)
-                1.0f, 0.0f, 0.0f,
-                1.0f, 0.0f, 0.0f,
-                1.0f, 0.0f, 0.0f,
-                1.0f, 0.0f, 0.0f,
-        };
-        int[] indices = new int[]{
-            // Front face
-            1, 0, 2, 3, 2, 0,
-            // Top face
-            6, 7, 4, 5, 4, 7,
-            // Back face
-            8, 9, 10, 11, 10, 9,
-            // Bottom face
-            12, 13, 15, 14, 15, 13,
-            // Left face
-            16, 17, 18, 19, 18, 17,
-            // Right face
-            20, 22, 21, 23, 21, 22,
-        };
-        Mesh mesh = new Mesh(positions, colours, indices);
-        GameItem gameItem1 = new GameItem(mesh);
-        gameItem1.setScale(0.5f);
-        gameItem1.setPosition(0, 0, -2);
-
-        gameItems = new GameItem[]{gameItem1};
+        gameItems = new GameItem[]{cube1, cube2};
     }
     
 
