d656c4b53ed2e7121c8fbd094e3f1f592658572f
Florian Peeters <florian@novito.be>
2017-10-18 13:33:23 +0200

    finished screenshots to byte array

--- src/image/ImageCreator.java
+++ src/image/ImageCreator.java
@@ -18,7 +18,7 @@ public class ImageCreator {
     /**
      * Based on
      */
-    public void screenShot(){
+    public byte[] screenShot(){
         int multiplier;
         String osName = System.getProperty("os.name");
         if ( osName.contains("Mac") ) {
@@ -26,8 +26,6 @@ public class ImageCreator {
         } else {
             multiplier = 1;
         }
-
-
 
         //Creating an rbg array of total pixels
         int[] pixels = new int[Constants.WIDTH * Constants.HEIGHT * multiplier * multiplier];
@@ -42,19 +40,13 @@ public class ImageCreator {
         for (int i=0; i < pixels.length; i++) {
             bindex = i * 3;
             pixels[i] =
-                    ((fb.get(bindex) << 16))  +
-                            ((fb.get(bindex+1) << 8))  +
-                            ((fb.get(bindex+2) << 0));
+                    ((fb.get(bindex)& 0xFF) << 16)  +
+                            ((fb.get(bindex+1) & 0xFF) << 8) +
+                            (fb.get(bindex+2)& 0xFF);
         }
         //Allocate colored pixel to buffered Image
-        BufferedImage imageIn = null;
-        try{
-            imageIn = new BufferedImage(Constants.WIDTH * multiplier, Constants.HEIGHT * multiplier,BufferedImage.TYPE_INT_RGB);
-            imageIn.setRGB(0, 0, Constants.WIDTH * multiplier, Constants.HEIGHT * multiplier, pixels, 0 , Constants.WIDTH * multiplier);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+        BufferedImage imageIn = new BufferedImage(Constants.WIDTH * multiplier, Constants.HEIGHT * multiplier,BufferedImage.TYPE_INT_RGB);
+        imageIn.setRGB(0, 0, Constants.WIDTH * multiplier, Constants.HEIGHT * multiplier, pixels, 0 , Constants.WIDTH * multiplier);
 
         //Creating the transformation direction (horizontal)
         AffineTransform at =  AffineTransform.getScaleInstance(1, -1);
@@ -63,12 +55,44 @@ public class ImageCreator {
         //Applying transformation
         AffineTransformOp opRotated = new AffineTransformOp(at, AffineTransformOp.TYPE_BILINEAR);
         BufferedImage imageOut = opRotated.filter(imageIn, null);
-
+        
         try {//Try to screate image, else show exception.
             ImageIO.write(imageOut, "png" , new File("ss.png"));
         }
         catch (Exception e) {
             System.out.println("ScreenShot() exception: " +e);
         }
+        
+        return (byte[]) imageOut.getData().getDataElements(0, 0, Constants.WIDTH * multiplier, Constants.HEIGHT * multiplier, new byte[Constants.WIDTH * multiplier * Constants.HEIGHT * multiplier * 3]);
     }
+    
+    public byte[] screenShotAlt(){
+    	byte[] pixels = new byte[Constants.WIDTH * Constants.HEIGHT * 3];
+
+        ByteBuffer fb = ByteBuffer.allocateDirect(Constants.WIDTH * Constants.HEIGHT * 3);
+        
+        glReadPixels(0, 0, Constants.WIDTH, Constants.HEIGHT, GL_RGB, GL_UNSIGNED_BYTE, fb);
+
+        for (int i=0; i < Constants.HEIGHT; i++) {
+        	for (int j=0; j < Constants.WIDTH; j++) {
+        		pixels[(i*Constants.WIDTH+j)*3] = fb.get(((Constants.HEIGHT-i-1)*Constants.WIDTH+j)*3);
+                pixels[(i*Constants.WIDTH+j)*3+1] = fb.get(((Constants.HEIGHT-i-1)*Constants.WIDTH+j)*3+1);
+                pixels[(i*Constants.WIDTH+j)*3+2] = fb.get(((Constants.HEIGHT-i-1)*Constants.WIDTH+j)*3+2);
+        	}
+        }
+        
+//		for exporting image.
+        BufferedImage imageOut = new BufferedImage(Constants.WIDTH, Constants.HEIGHT, BufferedImage.TYPE_3BYTE_BGR);
+		imageOut.getRaster().setDataElements(0, 0, Constants.WIDTH, Constants.HEIGHT, pixels);
+        
+        try {
+            ImageIO.write(imageOut, "png" , new File("ss.png"));
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        	System.out.println("ScreenShot() exception: " +e);
+        }
+        return pixels;
+    }
+    
 }
