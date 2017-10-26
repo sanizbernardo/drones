bb9e6041f03813ca5859a5b4a2d3c5f0f6143769
Florian Peeters <florian@novito.be>
2017-10-26 14:11:25 +0200

    added functionality for different color cubes

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -1,5 +1,6 @@
 package utils;
 
+import java.awt.Color;
 import java.io.InputStream;
 import java.util.Scanner;
 
@@ -23,4 +24,24 @@ public class Utils {
         return result;
     }
 
+    /**
+     * Converts a hsv color to rgb
+     * @param h
+     * 		 hue value, range: 0-360
+     * @param s
+     * 		 saturation value, range: 0-1
+     * @param v
+     * 		 value value ???, range: 0-1
+     * @return {r, g, b}
+     * 		 range 0-1 		  
+     */
+    public static float[] toRGB(int h, float s, float v) {
+    	Color rgb = new Color(Color.HSBtoRGB(h/360f, s, v));
+    	return new float[]{rgb.getRed()/255f, rgb.getGreen()/255f, rgb.getBlue()/255f};
+    }
+    
+    public static void main(String[] args) {
+    	
+	}
+    
 }
