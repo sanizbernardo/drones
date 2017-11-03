3f120518b07e18f44a8d517a2bab5be2efa8187d
Florian Peeters <jeanflo125@gmail.com>
2017-11-03 17:11:32 +0100

    implemented sliders for visial representation of autopilot outputs

--- src/gui/AutopilotGUI.java
+++ src/gui/AutopilotGUI.java
@@ -3,22 +3,27 @@ package gui;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.Hashtable;
-
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.plaf.metal.MetalSliderUI;
+
+import datatypes.AutopilotOutputs;
+
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import javax.swing.JSlider;
-import java.awt.GridLayout;
 
 public class AutopilotGUI extends JFrame {
 
@@ -27,14 +32,12 @@ public class AutopilotGUI extends JFrame {
 	private final int imgWidth, imgHeight;
 	
 	private JLabel lblImage;
-	private JPanel contentPane;
+	private JPanel topContentPanel;
 	private JPanel contentPanel;
 	private JPanel sliderPanel;
-	private JSlider lwSlider;
-	private JSlider horStabSlider;
-	private JSlider rwSlider;
-	private JSlider verStabSlider;
+	private SliderHandler lwSlider, horStabSlider, rwSlider, verStabSlider, thrustSlider;
 	private JPanel horSliderPanel;
+
 
 	/**
 	 * Launch the application.
@@ -43,7 +46,7 @@ public class AutopilotGUI extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					AutopilotGUI frame = new AutopilotGUI(200,200);
+					AutopilotGUI frame = new AutopilotGUI(200,200,1000);
 					frame.showGUI();
 					frame.lblImage.setIcon(new ImageIcon(ImageIO.read(new File("ss.png"))));;
 				} catch (Exception e) {
@@ -54,120 +57,83 @@ public class AutopilotGUI extends JFrame {
 	}
 
 	
-	
 	/**
 	 * Create the frame.
 	 */
-	public AutopilotGUI(int imgWidth, int imgHeight) {		
+	public AutopilotGUI(int imgWidth, int imgHeight, int maxThrust) {		
 		this.imgWidth = imgWidth;
 		this.imgHeight = imgHeight;
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		setTitle("Autopilot");
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+		topContentPanel = new JPanel();
+		topContentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		topContentPanel.setLayout(new BorderLayout(0, 0));
+		setContentPane(topContentPanel);
 		
 		JLabel lblAutopilotGui = new JLabel("Autopilot GUI");
 		lblAutopilotGui.setHorizontalAlignment(SwingConstants.CENTER);
-		contentPane.add(lblAutopilotGui, BorderLayout.NORTH);
+		topContentPanel.add(lblAutopilotGui, BorderLayout.NORTH);
 		
 		contentPanel = new JPanel();
-		contentPane.add(contentPanel, BorderLayout.CENTER);
+		topContentPanel.add(contentPanel, BorderLayout.CENTER);
 		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWeights = new double[]{1.0};
-		gbl_panel.rowWeights = new double[]{0.0};
 		contentPanel.setLayout(gbl_panel);
 		
+		
 		lblImage = new JLabel("");
-		GridBagConstraints gbc_lblImage = new GridBagConstraints();
-		gbc_lblImage.insets = new Insets(0, 0, 5, 0);
-		gbc_lblImage.anchor = GridBagConstraints.NORTHWEST;
-		gbc_lblImage.gridx = 0;
-		gbc_lblImage.gridy = 0;
-		contentPanel.add(lblImage, gbc_lblImage);
+		contentPanel.add(lblImage, buildGBC(0, 0, GridBagConstraints.CENTER, new Insets(5, 5, 5, 5)));
 		lblImage.setIcon(new ImageIcon());
 		lblImage.setPreferredSize(new Dimension(imgWidth, imgHeight));
 		
 		sliderPanel = new JPanel();
-		GridBagConstraints gbc_panel = new GridBagConstraints();
-		gbc_panel.fill = GridBagConstraints.BOTH;
-		gbc_panel.gridx = 1;
-		gbc_panel.gridy = 0;
-		contentPanel.add(sliderPanel, gbc_panel);
+		contentPanel.add(sliderPanel, buildGBC(1, 0, GridBagConstraints.CENTER));
 		GridBagLayout gbl_sliderPanel = new GridBagLayout();
-		gbl_sliderPanel.columnWeights = new double[]{0.0};
-		gbl_sliderPanel.rowWeights = new double[]{0.0, 0.0};
 		sliderPanel.setLayout(gbl_sliderPanel);
 		
+		
 		horSliderPanel = new JPanel();
-		GridBagConstraints gbc_horSliderPanel = new GridBagConstraints();
-		gbc_horSliderPanel.fill = GridBagConstraints.BOTH;
-		gbc_horSliderPanel.gridx = 0;
-		gbc_horSliderPanel.gridy = 0;
-		sliderPanel.add(horSliderPanel, gbc_horSliderPanel);
+		sliderPanel.add(horSliderPanel, buildGBC(0, 0, GridBagConstraints.CENTER));
 		GridBagLayout gbl_horSliderPanel = new GridBagLayout();
-		gbl_horSliderPanel.columnWidths = new int[] {20, 60, 60, 60};
-		gbl_horSliderPanel.columnWeights = new double[]{0.0, 0.0, 0.0};
-		gbl_horSliderPanel.rowWeights = new double[]{0.0};
+		gbl_horSliderPanel.columnWidths = new int[] {60, 60, 60};
 		horSliderPanel.setLayout(gbl_horSliderPanel);
+		
 		
 		Hashtable<Integer, JLabel> lblTable = new Hashtable<>();
 		for (int i=-90; i<=90; i+=30) {
-			lblTable.put(i, new JLabel("" + i));
+			JLabel lbl = new JLabel("" + i);
+			lbl.setFont(lbl.getFont().deriveFont(9.0f));
+			lblTable.put(i, lbl);
 		}
 		
-		lwSlider = new JSlider(SwingConstants.VERTICAL, -90, 90, 0);
-		GridBagConstraints gbc_lwSlider = new GridBagConstraints();
-		gbc_lwSlider.anchor = GridBagConstraints.NORTHWEST;
-		gbc_lwSlider.insets = new Insets(0, 0, 0, 5);
-		gbc_lwSlider.gridx = 1;
-		gbc_lwSlider.gridy = 0;
-		horSliderPanel.add(lwSlider, gbc_lwSlider);
-		lwSlider.setPaintTicks(true);
-		lwSlider.setMajorTickSpacing(15);
-		lwSlider.setMinorTickSpacing(5);
-		lwSlider.setLabelTable(lblTable);
-		lwSlider.setPaintLabels(true);
+		
+		lwSlider = buildSlider("RW", SwingConstants.VERTICAL, -90, 0, 90, 30, 10, lblTable, false);
+		horSliderPanel.add(lwSlider.panel, buildGBC(0, 0, GridBagConstraints.CENTER, new Insets(0, 5, 0, 5)));
 		
 		
-		horStabSlider = new JSlider(SwingConstants.VERTICAL, -90, 90, 0);
-		GridBagConstraints gbc_horStabSlider = new GridBagConstraints();
-		gbc_horStabSlider.anchor = GridBagConstraints.NORTHWEST;
-		gbc_horStabSlider.insets = new Insets(0, 0, 0, 5);
-		gbc_horStabSlider.gridx = 2;
-		gbc_horStabSlider.gridy = 0;
-		horSliderPanel.add(horStabSlider, gbc_horStabSlider);
-		horStabSlider.setPaintTicks(true);
-		horStabSlider.setMajorTickSpacing(15);
-		horStabSlider.setMinorTickSpacing(5);
-		horStabSlider.setLabelTable(lblTable);
-		horStabSlider.setPaintLabels(true);
+		horStabSlider = buildSlider("HS", SwingConstants.VERTICAL, -90, 0, 90, 30, 10, lblTable, false);
+		horSliderPanel.add(horStabSlider.panel, buildGBC(1, 0, GridBagConstraints.CENTER, new Insets(0, 5, 0, 5)));
 		
 		
-		rwSlider = new JSlider(SwingConstants.VERTICAL, 0, 10, 5);
-		GridBagConstraints gbc_rwSlider = new GridBagConstraints();
-		gbc_rwSlider.anchor = GridBagConstraints.NORTHWEST;
-		gbc_rwSlider.gridx = 3;
-		gbc_rwSlider.gridy = 0;
-		horSliderPanel.add(rwSlider, gbc_rwSlider);
+		rwSlider = buildSlider("LW", SwingConstants.VERTICAL, -90, 0, 90, 30, 10, lblTable, false);
+		horSliderPanel.add(rwSlider.panel, buildGBC(2, 0, GridBagConstraints.CENTER, new Insets(0, 5, 0, 5)));
 		
 
-		verStabSlider = new JSlider(SwingConstants.HORIZONTAL, -90, 90, 0);
-		GridBagConstraints gbc_verStabSlider = new GridBagConstraints();
-		gbc_verStabSlider.insets = new Insets(0, 0, 5, 0);
-		gbc_verStabSlider.fill = GridBagConstraints.BOTH;
-		gbc_verStabSlider.gridx = 0;
-		gbc_verStabSlider.gridy = 1;
-		sliderPanel.add(verStabSlider, gbc_verStabSlider);
-		verStabSlider.setPaintTicks(true);
-		verStabSlider.setMajorTickSpacing(15);
-		verStabSlider.setMinorTickSpacing(5);
-		verStabSlider.setLabelTable(lblTable);
-		verStabSlider.setPaintLabels(true);
+		verStabSlider = buildSlider("VS", SwingConstants.HORIZONTAL, -90, 0, 90, 30, 10, lblTable, false); 
+		sliderPanel.add(verStabSlider.panel, buildGBC(0, 1, GridBagConstraints.CENTER, new Insets(5, 0, 5, 5)));
 		
+		
+		Hashtable<Integer, JLabel> thrustTable = new Hashtable<>();
+		thrustTable.put(0, new JLabel("0"));
+		thrustTable.put(maxThrust/2, new JLabel(""+maxThrust/2));
+		thrustTable.put(maxThrust, new JLabel(""+maxThrust));
+		
+		thrustSlider = buildSlider("Thrust", SwingConstants.VERTICAL, 0, 0, maxThrust, maxThrust/10, maxThrust/40, thrustTable, true);
+		thrustSlider.slider.setPreferredSize(new Dimension(57, 270));
+		GridBagConstraints gbc_thrustSlider = buildGBC(1, 0, GridBagConstraints.CENTER, new Insets(0, 5, 0, 5));
+		gbc_thrustSlider.gridheight = 2;
+		sliderPanel.add(thrustSlider.panel, gbc_thrustSlider);
 	}
 	
 	
@@ -181,5 +147,98 @@ public class AutopilotGUI extends JFrame {
 		img.getRaster().setDataElements(0, 0, imgWidth, imgHeight, image);
 		lblImage.setIcon(new ImageIcon(img));
 	}
+	
+	public void updateOutputs(AutopilotOutputs output) {
+		lwSlider.setValue((int) Math.toDegrees(output.getLeftWingInclination()));
+		rwSlider.setValue((int) Math.toDegrees(output.getRightWingInclination()));
+		horStabSlider.setValue((int) Math.toDegrees(output.getHorStabInclination()));
+		verStabSlider.setValue((int) Math.toDegrees(output.getVerStabInclination()));
+		thrustSlider.setValue((int) Math.toDegrees(output.getThrust()));
+	}
 
+	
+	private static SliderHandler buildSlider(String title, int orientation, int min, int val, int max, int majorSpacing, int minorSpacing, Hashtable<Integer, JLabel> lblTable, boolean hasBar) {
+		JPanel panel = new JPanel();
+		GridBagLayout gbl = new GridBagLayout();
+		panel.setLayout(gbl);
+		
+		JLabel titleLbl = new JLabel(title);
+		panel.add(titleLbl, buildGBC(0, 0, GridBagConstraints.CENTER));
+		
+		JSlider slider = new JSlider(orientation, min, max, val);
+		slider.setPaintTicks(true);
+		slider.setMajorTickSpacing(majorSpacing);
+		slider.setMinorTickSpacing(minorSpacing);
+		slider.setLabelTable(lblTable);
+		slider.setPaintLabels(true);
+		if (!hasBar)
+			slider.setUI(new NoBarMetalSliderUI());
+		panel.add(slider, buildGBC(0, 1, GridBagConstraints.CENTER));
+		
+		JLabel label = new JLabel("" + val);
+		panel.add(label, buildGBC(0, 2, GridBagConstraints.CENTER));
+		
+		SliderHandler handler = new SliderHandler(slider, panel, label);
+		slider.addChangeListener(handler);
+		return handler;
+	}
+	
+	private static GridBagConstraints buildGBC(int gridx, int gridy, int anchor) {
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.anchor = anchor;
+		gbc.gridx = gridx;
+		gbc.gridy = gridy;
+		return gbc;
+	}
+	
+	private static GridBagConstraints buildGBC(int gridx, int gridy, int anchor, Insets insets) {
+		GridBagConstraints gbc = buildGBC(gridx, gridy, anchor);
+		gbc.insets = insets;
+		return gbc;
+	}
+	
+	
+	private static class SliderHandler implements ChangeListener {
+
+		private final JSlider slider;
+		private final JPanel panel;
+		private final JLabel label;
+		private int val;
+		public boolean lock = true;
+		
+		public SliderHandler(JSlider slider, JPanel panel, JLabel label) {
+			this.slider = slider;
+			this.panel = panel;
+			this.label = label;
+			this.val = slider.getValue();
+		}
+		
+		public void stateChanged(ChangeEvent e) {
+			if (lock) {
+				slider.setValue(val);
+			} else {
+				val = slider.getValue();
+				label.setText("" + val);
+			}
+		}
+		
+		public void setValue(int val) {
+			boolean oldLock = lock;
+			lock = false;
+			slider.setValue(val);
+			lock = oldLock;
+		}
+	}
+	
+	/**
+	 * shady workaround voor iets dat eigenlijk onnodig is.
+	 */
+	private static class NoBarMetalSliderUI extends MetalSliderUI {
+		
+		@Override
+		public void paintTrack(Graphics g) {
+			super.filledSlider = false;
+			super.paintTrack(g);
+		}
+	}
 }
