23a8e1f8a00895a00349b8308f63040b70b6bcae
Florian Peeters <jeanflo125@gmail.com>
2018-02-27 16:28:39 +0100

    more logging

--- src/world/World.java
+++ src/world/World.java
@@ -26,6 +26,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.lang.ref.PhantomReference;
 import java.util.ArrayList;
 
 
@@ -124,7 +125,7 @@ public abstract class World implements IWorldRules {
     	try {
 			File file = new File("position.log");
 			writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file)));
-			writer.write("position log, x  y  z  heading  pitch  roll\n");
+			writer.write("position log, x  y  z  heading  pitch  roll lincl hincl rincl vinlc thrust\n");
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -166,7 +167,9 @@ public abstract class World implements IWorldRules {
         if (writer != null) {
 	        try {
 				writer.write(time + ": " + physics.getPosition().x + " " + physics.getPosition().y + " " + physics.getPosition().z +
-							" " + physics.getHeading() + " " + physics.getPitch() + " " + physics.getRoll() + "\n");
+							" " + physics.getHeading() + " " + physics.getPitch() + " " + physics.getRoll() + 
+							" " + physics.getLWInclination() + " " + physics.getHSInclination() + " " + 
+							physics.getRWInclination() + " " + physics.getVSInclination() + " " + physics.getThrust() + "\n");
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
