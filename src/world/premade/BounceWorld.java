2eca962af0582504e90e7a6ce8254da72c44e64b
Arno Coomans <arno.coomans@student.kuleuven.be>
2018-02-27 18:55:37 +0100

    fixed some labels

--- src/world/premade/BounceWorld.java
+++ src/world/premade/BounceWorld.java
@@ -4,6 +4,7 @@ import org.joml.Vector3f;
 
 import entities.WorldObject;
 import physics.Physics;
+import pilot.Pilot;
 import utils.Utils;
 import world.World;
 
@@ -20,7 +21,7 @@ public class BounceWorld extends World {
 		this.physics = new Physics(false);
 		this.physics.init(config, new Vector3f(0, -config.getWheelY() + config.getTyreRadius(), 0), new Vector3f(0,-1.8f,0));
 		
-		this.planner = null;
+		this.planner = new Pilot();
 		
 		this.worldObjects = new WorldObject[0];
 	}
