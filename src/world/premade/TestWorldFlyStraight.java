93de79b5ef0e61c09f7d0b062d34e8f1e978f3fe
Arno Coomans <arno.coomans@student.kuleuven.be>
2018-02-27 16:33:52 +0100

    los in de broek

--- src/world/premade/TestWorldFlyStraight.java
+++ src/world/premade/TestWorldFlyStraight.java
@@ -9,32 +9,31 @@ import utils.Cubes;
 import utils.Utils;
 import world.World;
 
-
 /**
  * Place where all the GameItem are to be placed in
  */
 public class TestWorldFlyStraight extends World implements IWorldRules {
 
-    public TestWorldFlyStraight() {
-        super(1, true);
-    }
+	public TestWorldFlyStraight() {
+		super(1, true);
+	}
 
-    /**
-     * Is called in the abstract class
-     */
-    @Override
-    public void setup() {
-    	config = Utils.createDefaultConfig();
-    	  
-    	physics.init(config,new Vector3f(0,100,0), 10f);
+	/**
+	 * Is called in the abstract class
+	 */
+	@Override
+	public void setup() {
+		config = Utils.createDefaultConfig();
 
-    	planner = new Pilot();
-    	
-        worldObjects = new WorldObject[1];
-        worldObjects[0] = new WorldObject(Cubes.getCubes()[0].getMesh());
-        worldObjects[0].setPosition(0f,100f,-100f);
+		physics.init(config, new Vector3f(0, 100, 0), 10f);
 
-    }
+		planner = new Pilot();
+
+		worldObjects = new WorldObject[1];
+		worldObjects[0] = new WorldObject(Cubes.getCubes()[0].getMesh());
+		worldObjects[0].setPosition(0f, 100f, -100f);
+
+	}
 
 	@Override
 	public String getDescription() {
