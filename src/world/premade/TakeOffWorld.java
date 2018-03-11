7d6f2b9ac0a462b0bf427dcbed2c07fed1097c91
flortheuns <flor.theuns@hotmail.com>
2018-03-11 17:07:19 +0100

    physics bug

--- src/world/premade/TakeOffWorld.java
+++ src/world/premade/TakeOffWorld.java
@@ -7,6 +7,7 @@ import entities.ground.Ground;
 import entities.tarmac.Tarmac;
 import pilot.Pilot;
 import utils.Cubes;
+import utils.FloatMath;
 import utils.Utils;
 import world.World;
 
@@ -20,7 +21,7 @@ public class TakeOffWorld extends World {
 	public void setup() {
 		this.config = Utils.createDefaultConfig();
 		
-		addDrone(config, new Vector3f(0, -config.getWheelY() + config.getTyreRadius(), 0), new Vector3f(0,0,0));
+		addDrone(config, new Vector3f(0, -config.getWheelY() + config.getTyreRadius(), 0), new Vector3f(0,0,0), FloatMath.toRadians(90));
 		
 		this.planner = new Pilot(new int[] {Pilot.TAKING_OFF});
 		
