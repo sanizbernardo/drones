c1d5098d687b8379b3b49524bc4aba5ddc500278
Arno Coomans <arno.coomans@student.kuleuven.be>
2018-03-26 23:11:17 +0200

    basicly finished

--- src/interfaces/AutopilotFactory.java
+++ src/interfaces/AutopilotFactory.java
@@ -4,6 +4,6 @@ import pilot.Pilot;
 
 public class AutopilotFactory {
 	public static Autopilot createAutopilot() {
-		return new Pilot(new int[] {Pilot.WAIT_PATH, Pilot.TAKING_OFF,  Pilot.FLYING, Pilot.LANDING});
+		return new Pilot(new int[] {Pilot.WAIT_PATH, Pilot.TAKING_OFF,  Pilot.FLYING, Pilot.LANDING, Pilot.TAXIING});
 	}
 }
